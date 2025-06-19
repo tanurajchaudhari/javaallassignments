8708c648fc57a112efddc0d4d629e28bf7f291e3
tanuraj <tanuraj169179@gmail.com>
2025-06-19 17:18:27 +0530

    push

---
+++ cntstmnts/squarepattern.java
@@ -0,0 +1,53 @@
+//Q.1)   D  C  B  A
+//       4  4  4  4
+//       D  C  B  A
+//       4  4  4  4
+
+/*
+import java.io.*;
+class one{
+
+	public static void main(String[]args)throws IOException{
+	
+		BufferedReader br=new BufferedReader(new InputStreamReader(System.in));
+		System.out.println("Enter the number of rows : - ");
+		int N=Integer.parseInt(br.readLine());
+		
+	
+
+		for(int i=1;i<=N;i++){
+		        int num=N;
+			int ch=64+N;
+			for(int j=1;j<=N;j++){
+			
+				if(i%2==1){
+				
+					System.out.print((char)ch+"\t");
+					ch--;
+				}else{
+				
+					System.out.print(num+"\t");
+				}
+			}
+			System.out.println();
+
+		
+		}
+	
+	}
+}
+*/
+
+
+
+
+
+
+
+
+
+
+
+
+
+
